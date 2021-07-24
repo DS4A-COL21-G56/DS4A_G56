e9136ce62568e1a40f00185650f1f88e3b77cd30
Alejandro Marín Cardona <almarinca@unal.edu.co>
2021-07-24 15:46:36 -0500

    first dash interactive features

--- src/app/app.py
+++ src/app/app.py
@@ -2,9 +2,12 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
+import numpy as np
+from dash.dependencies import Output, Input
 
 df = pd.read_csv('data/cleaned/perfil_ingreso_v2.csv')
-data = df.groupby('COLEGIO_PROCEDENCIA').CODIGO.count().to_frame().sample(10).reset_index()
+# data = df.groupby('COLEGIO_PROCEDENCIA').CODIGO.count().to_frame().sample(10).reset_index()
+data = df
 
 external_stylesheets = [
     {
@@ -35,60 +38,65 @@ app.layout = html.Div(
         html.Div(
             children=[
                 html.Div(
-                    children=dcc.Graph(
-                        id="highschool-barplot",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": data["COLEGIO_PROCEDENCIA"],
-                                    "y": data["CODIGO"],
-                                    "type": "bar",
-                                    "hovertemplate": "%{y:d}"
-                                                     "<extra></extra>",
-                                },
+                    children=[
+                        html.Div(children="Column", className="menu-title"),
+                        dcc.Dropdown(
+                            id="column-filter",
+                            options=[
+                                {"label": col, "value": col}
+                                for col in ['GENERO', 'EDAD', 'COLEGIO_PROCEDENCIA']
                             ],
-                            "layout": {
-                                "title": {
-                                    "text": "High schools",
-                                    "x": 0.05,
-                                    "xanchor": "left",
-                                },
-                                #"height": 700,  # px
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {
-                                    #"tickprefix": "$",
-                                    "fixedrange": True,
-                                },
-                                "colorway": ["#17B897"],
-                            },
-                        },
+                            value="GENERO",
+                            clearable=False,
+                            className="dropdown",
+                        ),
+                    ]
+                ),
+                html.Div(
+                    children=[
+                        html.Div(children="Sample", className="menu-title"),
+                        dcc.Dropdown(
+                            id="sample-filter",
+                            options=[
+                                {"label": str(n_sample), "value": n_sample}
+                                for n_sample in range(1,31)
+                            ],
+                            value=5,
+                            clearable=False,
+                            searchable=False,
+                            className="dropdown",
+                        ),
+                    ],
+                ),
+                # html.Div(
+                    # children=[
+                        # html.Div(
+                            # children="Date Range",
+                            # className="menu-title"
+                            # ),
+                        # dcc.DatePickerRange(
+                            # id="date-range",
+                            # min_date_allowed=data.Date.min().date(),
+                            # max_date_allowed=data.Date.max().date(),
+                            # start_date=data.Date.min().date(),
+                            # end_date=data.Date.max().date(),
+                        # ),
+                    # ]
+                # ),
+            ],
+            className="menu",
+        ),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="highschool-barplot", config={"displayModeBar": False},
                     ),
                     className="card",
                 ),
                 html.Div(
                     children=dcc.Graph(
-                        id="volume-chart",
-                        config={"displayModeBar": False},
-                        figure={
-                            "data": [
-                                {
-                                    "x": data["COLEGIO_PROCEDENCIA"],
-                                    "y": data["CODIGO"],
-                                    "type": "lines",
-                                },
-                            ],
-                            "layout": {
-                                "title": {
-                                    "text": "High schools, but a line",
-                                    "x": 0.05,
-                                    "xanchor": "left",
-                                },
-                                "xaxis": {"fixedrange": True},
-                                "yaxis": {"fixedrange": True},
-                                "colorway": ["#E12D39"],
-                            },
-                        },
+                        id="highschool-lineplot", config={"displayModeBar": False},
                     ),
                     className="card",
                 ),
@@ -97,6 +105,68 @@ app.layout = html.Div(
         ),
     ]
 )
+
+
+@app.callback(
+    [Output("highschool-barplot", "figure"), Output("highschool-lineplot", "figure")],
+    [
+        Input("column-filter", "value"),
+        Input("sample-filter", "value"),
+        # Input("date-range", "start_date"),
+        # Input("date-range", "end_date"),
+    ],
+)
+def update_charts(col, n_sample):#, start_date, end_date):
+    # mask = (
+        # (data.col == col)
+        # & (data.type == sample)
+        # & (data.Date >= start_date)
+        # & (data.Date <= end_date)
+    # )
+    # filtered_data = data.loc[mask, :]
+    if col == 'GENERO':
+        filtered_data = data.groupby(col).CODIGO.count().to_frame().reset_index()
+    else:
+        filtered_data = data.groupby(col).CODIGO.count().to_frame().sample(n_sample).reset_index()
+
+    barplot_figure = {
+        "data": [
+            {
+                "x": filtered_data[col],
+                "y": filtered_data["CODIGO"],
+                "type": "bar",
+                "hovertemplate": "%{y:d}<extra></extra>",
+            },
+        ],
+        "layout": {
+            "title": {
+                "text": "High schools",
+                "x": 0.05,
+                "xanchor": "left",
+            },
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"tickprefix": "", "fixedrange": True},
+            "colorway": ["#17B897"],
+        },
+    }
+
+    lineplot_figure = {
+        "data": [
+            {
+                "x": filtered_data[col],
+                "y": filtered_data["CODIGO"],
+                "type": "lines",
+            },
+        ],
+        "layout": {
+            "title": {"text": "High schools, but a line", "x": 0.05, "xanchor": "left"},
+            "xaxis": {"fixedrange": True},
+            "yaxis": {"fixedrange": True},
+            "colorway": ["#E12D39"],
+        },
+    }
+    return barplot_figure, lineplot_figure
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
