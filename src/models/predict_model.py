913696779dc5e735b972a6a6ab4f355ac5761a59
Ricardo <raib1997@gmail.com>
2021-08-15 09:26:16 -0500

    predict_model.py is now working

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -13,12 +13,29 @@ import pickle
 
 SEED = 1111
 
+rf_model_file = '../../models/best_random_model.pkl'
+
 with open(rf_model_file, 'rb') as file:
     best_random_model = pickle.load(file)
     
-dataset_2021 = pd.read_csv('../../data/processed/final_dataset_distinct.csv', index_col = 0)
-training_dataset = pd.read_csv('../../data/processed/extended_dataset_final.csv', index_col = 0)
 
+
+training_dataset = pd.read_csv('../../data/processed/extended_dataset_final.csv', index_col = 0)
 training_dummy = pd.get_dummies(training_dataset.drop(['CODIGO', 'PERIODO_ACADEMICO'], axis = 1), drop_first = True)
 
+
 student_code = input("Please enter a student's code:\n")
+student_code = int(student_code)
+
+
+student_df = training_dataset.copy()
+
+student_df = student_df[student_df['CODIGO'] == student_code].reset_index()
+student_row = student_df.iloc[student_df['n_semesters'].idxmax():1,:]
+
+student_row_dummy = pd.get_dummies(student_row.drop(['CODIGO', 'PERIODO_ACADEMICO'], axis = 1))
+student_row_dummy = student_row_dummy.reindex(columns = training_dummy.columns, fill_value=0)
+
+result = best_random_model.predict_proba(student_row_dummy.drop(['ES_DESERTOR_SI'], axis = 1).values)[0][1]*100
+
+print('This student has {:.3f}% probabilities to drop out'.format(result))
